c9c0e1ff3aba31e63746e3fe6b2bcbe2b6253394
nmorrone <n.morrone96@gmail.com>
2024-11-28 15:50:03 +0100

    User-Roles-DUD-methods

--- src/main/java/is/lessons/spring/pizzeria/model/Role.java
+++
@@ -1,5 +0,0 @@
-package is.lessons.spring.pizzeria.model;
-
-public class Role {
-
-}
--- src/main/java/is/lessons/spring/pizzeria/security/DatabaseUserDetails.java
+++ src/main/java/is/lessons/spring/pizzeria/security/DatabaseUserDetails.java
@@ -1,11 +1,39 @@
 package is.lessons.spring.pizzeria.security;
 
 import java.util.Collection;
+import java.util.HashSet;
 
+import org.apache.catalina.Role;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import is.lessons.spring.pizzeria.model.Roles;
+import is.lessons.spring.pizzeria.model.User;
+
 public class DatabaseUserDetails implements UserDetails {
+	
+	
+	private final Integer id;
+	private final String username;
+	private final String password;
+	private final HashSet<GrantedAuthority> authorities;
+	
+	
+	public DatabaseUserDetails(User user) {
+		
+		this.id = user.getId();
+		this.username = user.getUsername();
+		this.password = user.getPassword();
+		authorities = new HashSet<GrantedAuthority>();
+		
+		for(Roles role : user.getRoles()) {
+			
+			authorities.add(new SimpleGrantedAuthority(role.getName()));
+		}
+	}
+	
+	
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -23,6 +51,30 @@ public class DatabaseUserDetails implements UserDetails {
 	public String getUsername() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return UserDetails.super.isAccountNonExpired();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return UserDetails.super.isAccountNonLocked();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return UserDetails.super.isCredentialsNonExpired();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return UserDetails.super.isEnabled();
 	}
 
 }
